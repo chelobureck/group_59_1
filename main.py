17687115a50205029f2a27c9ef097793c95951a0
chelobureck <adylbekoveldiyar@gmail.com>
2025-10-31 18:51:23 +0600

    fr com

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 # 2. Десктопные приложения 1 ч.  Знакомство PyQt, Flet или Тэкинтер. Понятия Декомпозиция.
+import flet as ft
 
-print("Hello world!")
+def main(page: ft.Page):
+    page.title = "Мое первое приложение"
+    greeting_text = ft.Text("Hello world!")
+
+    name_input = ft.TextField(label="Введите имя")
+
+    name_button = ft.ElevatedButton("send", icon=ft.Icons.SEND)
+    name_button_text = ft.TextButton("send")
+    name_button_icon = ft.IconButton(icon=ft.Icons.SEND)
+
+    page.add(greeting_text, name_input, name_button, name_button_text, name_button_icon)
+
+
+
+ft.app(target=main, view=ft.WEB_BROWSER) #type: ignore
