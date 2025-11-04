2d8e90f416f5809d8ce4d19c94aecb4202f03bf9
chelobureck <adylbekoveldiyar@gmail.com>
2025-11-04 19:20:07 +0600

    lesson 3

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 # 2. Десктопные приложения 1 ч.  Знакомство PyQt, Flet или Тэкинтер. Понятия Декомпозиция.
 import flet as ft
+from datetime import datetime
 
 def main(page: ft.Page):
     page.title = "Мое первое приложение"
     greeting_text = ft.Text("Hello world!")
+    greeting_history = []
+    history_text = ft.Text("История приветствий")
 
-    name_input = ft.TextField(label="Введите имя")
+    def on_button_click(_):
+        name = name_input.value.strip() #type: ignore
+        timestap = datetime.now().strftime("%H:%M")
+        if name:
+            greeting_text.value = f"{timestap} Hello {name}"
+            name_input.value = None
 
-    name_button = ft.ElevatedButton("send", icon=ft.Icons.SEND)
-    name_button_text = ft.TextButton("send")
-    name_button_icon = ft.IconButton(icon=ft.Icons.SEND)
+            greeting_history.append(f"{timestap} - {name}")
+            history_text.value = "История приветствий \n" + "\n".join(greeting_history)
+        else:
+            greeting_text.value = "Введите корректное имя"
+            greeting_text.color = ft.Colors.RED
+        page.update()
 
-    page.add(greeting_text, name_input, name_button, name_button_text, name_button_icon)
+    name_input = ft.TextField(label="Введите имя", on_submit=on_button_click)
 
+    name_button = ft.ElevatedButton("send", icon=ft.Icons.SEND, on_click=on_button_click)
+    # name_button_text = ft.TextButton("send")
+    # name_button_icon = ft.IconButton(icon=ft.Icons.SEND)
+
+    page.add(greeting_text, name_input, name_button) # name_button_text, name_button_icon
 
 
 ft.app(target=main) #type: ignore
